a3c42d45d618528a4953e2db313a483c1b52b21b
jeyin-nuary <122019196+jeyin-nuary@users.noreply.github.com>
2023-08-08 23:20:13 +0900

    feat: 보드 기본적인 CRUD api 구현

--- controllers/boards.controller.js
+++ controllers/boards.controller.js
@@ -1,6 +1,66 @@
 const BoardsService = require('../services/boards.service');
+const color = require('../models/board');
 
 class BoardsController {
   boardsService = new BoardsService();
+
+  handleError(res, error) {
+    if (error.code) {
+      return res.status(error.code).json({ message: error.message });
+    }
+    console.error(error);
+    return res.status(500).send('알 수 없는 에러가 발생');
+  }
+
+  // 보드 생성
+  createBoard = async (req, res) => {
+    try {
+      const { name, description } = req.body;
+      // const {userId} = res.locals.user;
+      const { code, message } = await this.boardsService.createBoard({
+        userId,
+        name,
+        color,
+        description,
+      });
+      return res.status(code).json({ message });
+    } catch (error) {
+      return this.handleError(res, error);
+    }
+  };
+
+  // 보드 수정
+  updateBoard = async (req, res) => {
+    try {
+      const { name, description } = req.body;
+      const { boardId } = req.params;
+      const { userId } = res.locals.user;
+      const { code, message } = await this.boardsService.updateBoard({
+        boardId,
+        userId,
+        name,
+        description,
+        color,
+      });
+      return res.status(code).json({ message });
+    } catch (error) {
+      return this.handleError(res, error);
+    }
+  };
+
+  // 보드 삭제
+  deleteBoard = async (req, res) => {
+    try {
+      const { boardId } = req.params;
+      const { userId } = res.locals.user;
+      const { code, message } = await this.boardsService.deleteBoard({
+        boardId,
+        userId,
+      });
+      return res.status(code).json({ message });
+    } catch (error) {
+      return this.handleError(res, error);
+    }
+  };
 }
 module.exports = BoardsController;
