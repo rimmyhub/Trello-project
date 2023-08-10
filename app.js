b056e3db8fe8aa4a215636dfc4c3ed04e0764f1e
yuni416 <131755987+yuni416@users.noreply.github.com>
2023-08-10 10:13:49 +0900

    0810

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const cookieParser = require('cookie-parser');
 const userRouter = require('./routes/users.router');
 const boardRouter = require('./routes/boards.router');
 const viewRouter = require('./views/router/index');
+const cardRouter = require('./routes/cards.router')
 
 const dotenv = require('dotenv');
 dotenv.config();
@@ -23,7 +24,7 @@ app.set('view', __dirname + '/views');
 app.use(express.static(__dirname + '/views/static'));
 app.use('/', viewRouter);
 
-app.use('/', [boardRouter, userRouter]);
+app.use('/', [boardRouter, userRouter,cardRouter]);
 
 app.listen(PORT, () => {
   console.log(PORT, '포트 번호로 서버가 실행되었습니다.');
