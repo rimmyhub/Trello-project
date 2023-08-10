4d4a31789399819bcb3f2b8ca4a72bbca2cb4062
Yehye2 <133616786+Yehye2@users.noreply.github.com>
2023-08-10 18:03:34 +0900

    병합 충돌 수정

--- controllers/columns.controller.js
+++ controllers/columns.controller.js
@@ -23,7 +23,6 @@ class ColumnsController {
     }
   };
 
-<<<<<<< HEAD
   getColumnById = async (req, res) => {
     const { columnId } = req.params;
     try {
@@ -48,27 +47,10 @@ class ColumnsController {
     }
   };
 
-<<<<<<< HEAD
   updateColumn = async (req, res) => {
     const { columnId } = req.params;
     const updatedData = req.body;
     const userId = res.locals.user.userId;
-=======
-    getColumnById = async (req, res) => {
-        const { columnId } = req.params;
-        try {
-            const column = await this.columnService.getColumnById(columnId);
-            if (column) {
-                res.status(200).json(column);
-            } else {
-                res.status(404).json({ message: '컬럼이 존재하지 않습니다.' });
-            }
-        } catch (error) {
-            console.error('Error retrieving columns:', error);
-            res.status(500).json({ error: '컬럼 조회 실패' });
-        }
-    };
->>>>>>> column
 
     try {
       const updatedColumn = await this.columnService.updateColumn(columnId, updatedData, userId);
