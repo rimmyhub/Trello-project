77186976134b72906fd90d58e14352374ae5132e
jeyin-nuary <122019196+jeyin-nuary@users.noreply.github.com>
2023-08-10 10:10:19 +0900

    feat: 보드 프론트 연결 시작

--- views/router/index.js
+++ views/router/index.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const UsersController = require('../../controllers/users.controller');
+const BoardsController = require('../../controllers/boards.controller');
 const router = express.Router();
 
 const usersController = new UsersController();
+const boardsController = new BoardsController();
+
+router.get('/');
 
 router.get('/login', (req, res) => {
   const data = await.usersController.logIn(email, password);
